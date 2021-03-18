c39468eb347ed07c8d83c73cd2896b3b42c78824
Christopher <onlygamernerd@gmail.com>
2021-03-18 15:49:20 +0100

    tweak(phone/bank): remove fontaswesome icons

--- phone/src/apps/bank/components/navigation/NavigationBar.tsx
+++ phone/src/apps/bank/components/navigation/NavigationBar.tsx
@@ -4,7 +4,9 @@ import { Link } from 'react-router-dom';
 import { BottomNavigation, BottomNavigationAction } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHome, faList, faMoneyCheck } from '@fortawesome/free-solid-svg-icons';
+import HomeIcon from '@material-ui/icons/Home';
+import LocalAtmIcon from '@material-ui/icons/LocalAtm';
+import ListIcon from '@material-ui/icons/List';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -30,21 +32,9 @@ export const NavigationBar = () => {
       showLabels
       className={classes.root}
     >
-      <BottomNavigationAction
-        component={Link}
-        icon={<FontAwesomeIcon icon={faHome} />}
-        to="/bank"
-      />
-      <BottomNavigationAction
-        component={Link}
-        icon={<FontAwesomeIcon icon={faMoneyCheck} />}
-        to="/bank/account"
-      />
-      <BottomNavigationAction
-        component={Link}
-        icon={<FontAwesomeIcon icon={faList} />}
-        to="/bank/transactions"
-      />
+      <BottomNavigationAction component={Link} icon={<HomeIcon />} to="/bank" />
+      <BottomNavigationAction component={Link} icon={<LocalAtmIcon />} to="/bank/account" />
+      <BottomNavigationAction component={Link} icon={<ListIcon />} to="/bank/transactions" />
     </BottomNavigation>
   );
 };
